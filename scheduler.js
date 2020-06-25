d70d304498a57d44da3722b881978276391b6806
Janella Senica <janella.senica@gmail.com>
2020-06-25 17:51:04 -0500

    initial version

---
+++ scheduler.js
@@ -0,0 +1,16 @@
+var businessHrs = {
+    "8AM": "",
+    "9AM": "",
+    "10AM": "",
+    "11AM": "",
+    "12PM": "",
+    "1PM": "",
+    "2PM": "",
+    "3PM": "",
+    "4PM": "",
+    "5PM": "",
+};
+
+$("#currentDay").text(moment().format('dddd, MMMM Do'));
+            console.log();
+        
